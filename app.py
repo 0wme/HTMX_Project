9cf160bbacc64089578135eb9946be8e6e379b5d
Tom <skixoxgaming@gmail.com>
2024-03-07 19:13:18 +0100

    Création de add_todo

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ todos = []  # Exemple: [{'content': 'Faire les courses', 'completed': False, 'fa
 @app.route('/')
 def todolist():
     return rendertemplate('todo_list.html', todos=todos)
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo_content = request.form.get('content')
+    if todo_content:
+        todos.append({'content': todo_content, 'completed': False, 'favorite': False})
+    return redirect(url_for('todo_list'))
+
 
 
 if __name__ == '__main':
